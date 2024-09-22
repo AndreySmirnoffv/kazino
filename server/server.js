b263753598b5cf6c08a60c78ae25c53c975fb66b
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-09-22 20:02:22 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -5,58 +5,86 @@ import { pool } from '../assets/db/connect.js';
 
 dotenv.config({ path: "../modules/.env" });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.locals.msg = '';
 
-app.post("/", (req, res) => {
-    app.locals.msg = req.body;
-    res.status(200).json({ message: app.locals.msg });
-});
+app.get('/user', (req, res) => {
+    const {username} = req.body
+    const query = `SELECT * WHERE username = ?`
 
-app.get("/", (_, res) => {
-    res.status(200).json({ message: app.locals.msg });
-});
-
-app.get('/user', (_, res) => {
-    const query = `SELECT (username, balance) WHERE username = ?`
-
-    pool.query(query, [app.locals.msg.from.username], (error, results) => {
+    pool.query(query, [username], (error, results) => {
         if (error) {
             return res.status(500).json({ error: error.message });
         }
 
-        const username = results[0].username
-        const balance = results[0].balance;
+        const user = results[0]
+        
 
-        res.status(200).json({ username, balance })
+        res.status(200).json({ user })
     })
 })
 
+
 app.post("/updaterank", async req => {
-    const { rank } = req.body;
+    const { rank, userId } = req.body;
     const query = `UPDATE users SET rank = ? WHERE id = ?`
 
-    pool.query(query, [rank, app.locals.msg.from.id])
+    pool.query(query, [rank, userId])
 });
 
 app.post("/updatebalance", async (req, res) => {
-    const { balance } = req.body;
-    if (!app.locals.msg || !app.locals.msg.from || !app.locals.msg.from.id) {
-        return res.status(400).json({ error: "User ID is not available" });
-    }
+    const { balance, userId } = req.body;
+
+   
     const query = `UPDATE users SET balance = ? WHERE id = ?`;
-    pool.query(query, [balance, app.locals.msg.from.id], (error, results) => {
+    pool.query(query, [balance, userId], error => {
         if (error) {
             return res.status(500).json({ error: error.message });
         }
         res.status(200).json({ message: "Balance updated successfully" });
     });
 });
+
+app.get('/top', (_, res) => {
+    const query = ` SELECT id, username, hourly_profit FROM users ORDER BY hourly_profit DESC LIMIT 100`
+    const rows = pool.query(query)
+
+    res.status(200).json({rows})
+})
+
+
+app.post("/addInventory", async (req, res) => {
+    const { cardName, cardAttributes } = req.body;
+
+    if ( !cardName || !cardAttributes) {
+        return res.status(400).json({ error: "Все поля обязательны" });
+    }
+
+    const query = `INSERT INTO inventory (userId, cardName, cardAttributes) VALUES (?, ?, ?)`;
+    pool.query(query, [app.locals.msg.from.id, cardName, JSON.stringify(cardAttributes)], (error) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        res.status(201).json({ message: "Запись добавлена в инвентарь" });
+    });
+});
+
+app.get('/getcards', async (_, res) => {
+    const query = `SELECT * FROM cards`;
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            console.error('Ошибка при выполнении запроса:', error);
+            return res.status(500).json({ error: 'Ошибка базы данных' });
+        }
+        res.json(results);
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Server is running on port http://127.0.0.1:${PORT}`));
